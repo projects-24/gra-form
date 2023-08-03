d485b684b76569c36caa7e018e6ccdad2093fecb
iddrismystic <abdulwahabiddris08@gmail.com>
2023-08-03 16:20:25 +0100

    i

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import 'funuicss/css/fun.css'
 import 'styles/globals.css'
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <Head>
+      <link rel="shortcut icon" href="/logo.png" />
+      <title>{`Tax relief application form (2023)`}</title>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
